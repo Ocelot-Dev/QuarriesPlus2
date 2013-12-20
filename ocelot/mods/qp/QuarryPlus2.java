695405e54f198fb278ec51e891106646d2d4033c
Peter <peter@werl.me>
2013-12-20 15:36:38 -0700

    one more liscence update

--- ocelot/mods/qp/QuarryPlus2.java
+++ ocelot/mods/qp/QuarryPlus2.java
@@ -1,5 +1,6 @@
 /*
- * Copyright (C) 2012,2013 yogpstop This program is free software: you can
+ * Copyright (C) 2012,2013 yogpstop: Updated by Werl and Snipe
+ * This program is free software: you can
  * redistribute it and/or modify it under the terms of the GNU Lesser General
  * Public License as published by the Free Software Foundation, either version 3
  * of the License, or (at your option) any later version. This program is
